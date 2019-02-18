f4afb9fa51f8c711ceadd85ff153ca70dc55df5c
Manikanta Rahul <manikantarahul.n@gmail.com>
2019-02-18 17:26:04 +0530

    javascript file

---
+++ javascript_anagrams.js
@@ -0,0 +1,47 @@
+function anagram_checker()
+{
+    var array = ['listen','silent','earth', 'heart'];		//input array
+    var output =[];											//output array
+	for (var i=0; i<array.length; i++)
+	{
+		st=array[i];
+		var alpha=[];
+		for (let k=0; k<st.length; k++)						//spliting words into chracters
+		{
+			alpha[k]=st.charAt(k);
+		}
+		for(var j=0; j<alpha.length; j++)	
+		{
+			for(var a=0; a<(alpha.length)-j-1; a++)			//sorting characters of a word
+			{
+				if(alpha[a]>alpha[a+1])
+				{
+					let s=alpha[a];
+					alpha[a]=alpha[a+1];
+					alpha[a+1]=s;
+				}
+					
+			}
+		}
+		var word="";
+		for(var b=0; b<alpha.length; b++)					// characters concatination
+		{
+			word=word+alpha[b];
+		}
+		output[i]=word;
+	}
+	for(var n=0; n<output.length;n=n+2)						//compare words
+    {
+        if(output[n]===output[n+1])
+        {
+            console.log(array[n]+' and '+array[n+1]+' are Anagrams');
+        }
+        else
+        {
+            console.log('They are not anagrams');
+        }
+                
+    }
+}
+
+anagram_checker();
